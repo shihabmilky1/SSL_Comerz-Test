337ed72750fceb813fc54ff559d1168e19fd4d62
shihabmilky1 <shihabmilky1@gmail.com>
2021-09-25 10:16:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+
+const SSLCommerzPayment = require('sslcommerz-lts')
+const store_id = 'shiha614e9921e28de'
+const store_passwd = 'shiha614e9921e28de@ssl'
+const is_live = false //true for live, false for sandbox
+
+const port = 5000
+
+
+app.get('/', (req, res) => {
+    res.send('Working')
+})
+
+//sslcommerz init
+app.get('/init', (req, res) => {
+    const data = {
+        total_amount: 100,
+        currency: 'BDT',
+        tran_id: 'REF123', // use unique tran_id for each api call
+        success_url: 'http://localhost:5000/success',
+        fail_url: 'http://localhost:5000/fail',
+        cancel_url: 'http://localhost:5000/cancel',
+        ipn_url: 'http://localhost:5000/ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: 'customer@example.com',
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+    };
+    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+    sslcz.init(data).then(apiResponse => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL
+        res.redirect(GatewayPageURL)
+        console.log('Redirecting to: ', GatewayPageURL)
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
